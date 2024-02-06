978487833aea3025564504066f18cfa64c12b13c
unknown <falakfatma510@gmail.com>
2024-02-06 19:57:08 +0530

    UI Is Added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import './App.css';
 import React, { Component,useEffect,useState } from 'react'
 
-const Data = localStorage()
+// const Data = 
 function App() {
-  const Date = new Date()
-  const [result,setResult]  = useState(111)
-  Data.add(22)
-  console.log(Data)
-  useEffect(()=>{
-      
-  },[result]) 
+  // const Date = new Date()
+  const [dateResult,setDateResult]  = useState(11)
+  const [diabetesNumberRecord,setDiabetesNumberRecord]  = useState(101)
+  localStorage.setItem("value",dateResult)
+  // console.log(Data)
+  // useEffect(()=>{
+      function setDateResultFunction(e) {
+        setDateResult(e.target.value)
+      }
+      function setResultInLocalStorage(params) {
+        localStorage.setItem(dateResult,diabetesNumberRecord)
+      }
+  // },[dateResult]) 
   return (
-  <div className='mainPage'>
-      <input placeholder='add date'/> 
-      <input placeholder='add Number'/> 
-  </div>
+    <div className='mainPage'>
+        <input className='inp_nav' onInput={setDateResultFunction} placeholder='Date-Month-Year'/> 
+        <input className='inp_nav' placeholder='Add Diabetes Number'/> 
+        <button onClick={setResultInLocalStorage}>Click</button>
+        <div className='box'>
+          <p>
+          Date : {dateResult}
+          </p> 
+          <p>
+            Number Result : {/* {numberResult}   */}
+          </p> 
+        </div> 
+    </div>
   );
 }
 
